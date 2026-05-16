6814d893efb29e06ca2ff851fce900bdecf3fc25
ankitkumar25-rk <ankitgia336@gmail.com>
2026-05-16 12:57:58 +0530

    fix: use relative path for google callback

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: process.env.GOOGLE_CALLBACK_URL,
+        callbackURL: '/api/auth/google/callback',
         proxy: true
     }, async (accessToken, refreshToken, profile, done) => {
         try {
