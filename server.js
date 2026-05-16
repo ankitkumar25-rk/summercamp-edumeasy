29c514e548cde2f91de3697457f3fa405574a7fd
ankitkumar25-rk <ankitgia336@gmail.com>
2026-05-16 12:53:27 +0530

    fix: set proxy true for google oauth

--- server.js
+++ server.js
@@ -29,7 +29,8 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: process.env.GOOGLE_CALLBACK_URL
+        callbackURL: process.env.GOOGLE_CALLBACK_URL,
+        proxy: true
     }, async (accessToken, refreshToken, profile, done) => {
         try {
             let user = await User.findOne({ googleId: profile.id });
